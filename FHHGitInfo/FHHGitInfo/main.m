df76841c53bdd2c220ce0c0464c29b4dd6d84fed
jvjishou <xxewz1@163.com>
2017-09-07 10:33:57 +0800

    intialize project

---
+++ FHHGitInfo/FHHGitInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FHHGitInfo
+//
+//  Created by hefanghui on 2017/9/7.
+//  Copyright © 2017年 002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
